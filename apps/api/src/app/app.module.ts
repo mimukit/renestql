76468f5c80e8e0542ebb6e97520965949eb6124b
Mukitul Islam Mukit <hellomukit@gmail.com>
2019-09-01 18:48:46 +0600

    refactor(api): add some comments to understand the setup

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -4,23 +4,29 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { getMetadataArgsStorage } from 'typeorm';
 import { typeOrmConfig } from '../config/typeorm/typeorm.config';
+import { environment } from '../environments/environment';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
+    // Database ORM Module (TypeORM)
     TypeOrmModule.forRoot({
       ...typeOrmConfig,
       entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
     }),
+
+    // Grpahql Server Module
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       definitions: {
         path: join(process.cwd(), './apps/api/src/app/graphql.types.ts'),
         outputAs: 'class',
       },
-      debug: true,
-      playground: true,
+      debug: environment.production ? false : true,
+      playground: environment.production ? false : true,
     }),
+
+    // Application Modules
     UserModule,
   ],
   controllers: [],
