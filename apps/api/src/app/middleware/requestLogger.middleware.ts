16bc45f043ec47d66e78d669492d81d762ae1a4d
Mukitul Islam Mukit <hellomukit@gmail.com>
2019-09-02 14:04:39 +0600

    feat(api): setup security & logger middleware

---
+++ apps/api/src/app/middleware/requestLogger.middleware.ts
@@ -0,0 +1,36 @@
+import { Logger } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+const generate = require('nanoid/generate');
+
+const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const generateRequestId = () => generate(alphabet, 25);
+
+export const requestLogger = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // Ignore introspection quries log
+  if (req.body && req.body.operationName === 'IntrospectionQuery') {
+    next();
+    return;
+  }
+
+  const requestId = generateRequestId();
+
+  res.set({
+    'X-Request-Id': requestId,
+  });
+
+  Logger.log(
+    `${requestId} ${req.method} ${
+      req.body.operationName === 'IntrospectionQuery'
+        ? 'Introspection Query'
+        : JSON.stringify(req.body)
+    }`,
+    'Request'
+  );
+
+  next();
+};
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,30 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
+import * as helmet from 'helmet';
 import { AppModule } from './app/app.module';
+import { requestLogger } from './app/middleware/requestLogger.middleware';
 import { environment } from './environments/environment';
 
 const PORT = environment.server.port;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Setup security middlewares
+  app.use(helmet());
+
+  app.enableCors();
+
+  // Setup logger middleware
+
+  app.use(bodyParser.json());
+
+  app.use(requestLogger);
+
+  // Start app
   await app.listen(PORT);
+
   Logger.log(
     `Server started at: ${
       environment.production
