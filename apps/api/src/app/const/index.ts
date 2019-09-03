4b60e74c7b1f6e39a31f0b6f1954042b75af3080
Mukitul Islam Mukit <hellomukit@gmail.com>
2019-09-03 13:38:23 +0600

    feat(api): add constants for api server

---
+++ apps/api/src/app/const/index.ts
@@ -0,0 +1,11 @@
+export const HASH_ROUND = 12;
+
+export const CUSTOM_ID_LENGTH = 10;
+export const CUSTOM_ID_CHARACTERS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const CUSTOM_ID_PREFIX = {
+  USER: 'USR',
+};
+
+export const API_ACCESS_KEY_LENGTH = 20;
+
+export const JWT_TOKEN_EXPIRE_TIME = '7d';
