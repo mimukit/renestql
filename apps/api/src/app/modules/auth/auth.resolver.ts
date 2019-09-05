0d0626ad057d5ae6e0022ef68e1d8a2b43e5ba8c
Mukitul Islam Mukit <hellomukit@gmail.com>
2019-09-05 13:33:00 +0600

    refactor(api): move register function to authService

--- apps/api/src/app/modules/auth/auth.resolver.ts
+++ apps/api/src/app/modules/auth/auth.resolver.ts
@@ -1,46 +1,18 @@
-import { BadRequestException, UsePipes } from '@nestjs/common';
+import { UsePipes } from '@nestjs/common';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { loginInputSchema, registerInputSchema } from '@nx-intro/schema';
 import { AuthPayload, LoginInput, RegisterInput } from '../../graphql.types';
 import { YupValidationPipe } from '../../pipes/yupValidation.pipe';
-import { UserRepository } from '../user/user.repository';
 import { AuthService } from './auth.service';
 
 @Resolver('Auth')
 export class AuthResolver {
-  constructor(
-    private readonly userRepository: UserRepository,
-    private readonly authService: AuthService
-  ) {}
+  constructor(private readonly authService: AuthService) {}
 
   @Mutation('register')
   @UsePipes(new YupValidationPipe(registerInputSchema))
-  async register(@Args('data') { email, password }: RegisterInput): Promise<
-    AuthPayload
-  > {
-    // Check if user with input email already exists
-    const userExists = await this.userRepository.findOne({ email });
-
-    if (userExists && userExists.id) {
-      throw new BadRequestException('User already exists with this email');
-    }
-
-    // Create new user with inputs
-
-    const hashPassword = await this.authService.getHashPassword(password);
-
-    const user = this.userRepository.create({ email, password: hashPassword });
-
-    const newUser = await this.userRepository.save(user);
-
-    // Generate login token for new user
-
-    const token = await this.authService.getToken(newUser);
-
-    return {
-      token,
-      user: newUser,
-    };
+  async register(@Args('data') data: RegisterInput): Promise<AuthPayload> {
+    return this.authService.register(data);
   }
 
   @Mutation('login')
--- apps/api/src/app/modules/auth/auth.service.ts
+++ apps/api/src/app/modules/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { BadRequestException, Injectable, Logger } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import * as bcrypt from 'bcryptjs';
 import { PASSWORD_HASH_ROUND } from '../../const';
-import { AuthPayload, LoginInput } from '../../graphql.types';
+import { AuthPayload, LoginInput, RegisterInput } from '../../graphql.types';
 import { User } from '../user/user.entity';
 import { UserRepository } from '../user/user.repository';
 
@@ -12,6 +12,32 @@ export class AuthService {
     private readonly userRepository: UserRepository,
     private readonly jwtService: JwtService
   ) {}
+
+  async register({ email, password }: RegisterInput): Promise<AuthPayload> {
+    // Check if user with input email already exists
+    const userExists = await this.userRepository.findOne({ email });
+
+    if (userExists && userExists.id) {
+      throw new BadRequestException('User already exists with this email');
+    }
+
+    // Create new user with inputs
+
+    const hashPassword = await this.getHashPassword(password);
+
+    const user = this.userRepository.create({ email, password: hashPassword });
+
+    const newUser = await this.userRepository.save(user);
+
+    // Generate login token for new user
+
+    const token = await this.getToken(newUser);
+
+    return {
+      token,
+      user: newUser,
+    };
+  }
 
   async login({ email, password }: LoginInput): Promise<AuthPayload> {
     // Check if user with input email exists''
